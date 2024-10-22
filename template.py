eb3d3ea422f8f371171db4210ba52159a2a35eec
Heera202 <detech1960@gmail.com>
2024-10-22 19:33:10 +0530

    packages installed

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'Dieases Classifier'
+project_name = 'Disease Classifier'
 
 list_of_files =[
     ".github/workflowws/.gitkeep",
